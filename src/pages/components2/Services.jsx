b075ccd6e5756a782e187df6e3ea7434bde2621c
sadam-codes <sadam.fullstackdev@gmail.com>
2025-05-11 22:00:13 +0500

    Update Services.jsx

--- src/pages/components2/Services.jsx
+++ src/pages/components2/Services.jsx
@@ -6,7 +6,7 @@ function Services() {
     <div className="container">
       <div className="index-section">
         <div className="services">
-          <h1>In the service of our people first initiative</h1>
+          <h1>giving service to the people is our first initiative</h1>
           <button className="services-button mx-auto btn-outline-success text-dark">
             Sign Up Now
           </button>
